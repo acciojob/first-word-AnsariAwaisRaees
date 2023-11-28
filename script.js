c362d1e804c024847f7838206b07d8696e587dbf
Awais Raees Ansari <80111573+AnsariAwaisRaees@users.noreply.github.com>
2023-11-28 17:36:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function firstWord(s) {
-  // your code here
+	// your code here
+	s = s.trimStart();
+	s = s.split(' ');
+
+	for (let index = 0; index < s.length; index++) {
+		return s[0];
+	}
 }
 
 // Do not change the code below
-
+ 
 const s = prompt("Enter String:");
 alert(firstWord(s));
